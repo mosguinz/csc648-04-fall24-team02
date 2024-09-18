428c21eccae61f8cce67b526824eb7139579e474
WeezMatt <124218015+WeezMatt@users.noreply.github.com>
2024-09-17 21:53:08 -0700

    Create Matthew.js

---
+++ application/frontend/public/Matthew.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Matthew() {
+  return (
+    <div className="Matthew">
+      <header className="Matthew-header">
+        <h1>Welcome to Matthew's Webpage</h1>
+        <h2>My Name: Matthew Weesner</h2>
+        <img src="/matt.jpg" alt="My Picture" style={{ width: '300px', borderRadius: '50%' }} />
+        <p>
+          This is a simple webpage displaying my name and picture.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Matthew;
