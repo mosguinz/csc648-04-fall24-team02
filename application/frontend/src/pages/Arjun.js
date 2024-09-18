09264d06c2704941ed647201fbe9e62ddd351dda
ArjunS132 <Arjunsgill132@Gmail.com>
2024-09-18 16:44:37 -0700

    set up about me page for Arjun

--- application/frontend/src/pages/Arjun.js
+++ application/frontend/src/pages/Arjun.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import ProfilePicture from '../assets/Arjun.jpg';
 function Arjun() {
   return (
     <div>
-      
+      <img
+        src={ProfilePicture}
+        alt="Matthew Weesner"
+        style={{width: '500px', height: 'auto'}}
+      />
+      <h1>Arjun Singh Gill</h1>
+      <h2>Backend Lead</h2>
     </div>
   )
 }
