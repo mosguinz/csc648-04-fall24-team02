a68b759948acbf1d27b758296b9ba87a9eefb209
Kiet Lam <klam11@sfsu.edu>
2024-09-18 16:21:48 -0700

    Styled About page witg bootstrap

--- application/frontend/src/pages/About.js
+++ application/frontend/src/pages/About.js
@@ -1,5 +1,8 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/About.css';
 
 function About() {
   const navigate = useNavigate();
@@ -9,18 +12,34 @@ function About() {
   };
 
   return (
-    <div>
-      <h1>About</h1>
-      <h2>Meet the Team!</h2>
+    <div className="container mt-5 custom-background">
+      <div className="text-center mb-4">
+        <h1>About</h1>
+        <h2>Meet the Team!</h2>
+      </div>
 
-      <div className="button-container">
-        <button onClick={() => handleClick('/Katy')}>Katy</button>
-        <button onClick={() => handleClick('/Kevin')}>Kevin</button>
-        <button onClick={() => handleClick('/Niko')}>Niko</button>
-        <button onClick={() => handleClick('/Arjun')}>Arjun</button>
-        <button onClick={() => handleClick('/Matthew')}>Matthew</button>
-        <button onClick={() => handleClick('/Mos')}>Mos</button>
-        <button onClick={() => handleClick('/Arizza')}>Arizza</button>
+      <div className="row">
+        <div className="col-12 mb-2">
+          <Button variant="primary" className="w-25 custom-button" onClick={() => handleClick('/Katy')}>Katy</Button>
+        </div>
+        <div className="col-12 mb-2">
+          <Button variant="secondary" className="w-25 custom-button" onClick={() => handleClick('/Kevin')}>Kevin</Button>
+        </div>
+        <div className="col-12 mb-2">
+          <Button variant="success" className="w-25 custom-button" onClick={() => handleClick('/Niko')}>Niko</Button>
+        </div>
+        <div className="col-12 mb-2">
+          <Button variant="danger" className="w-25 custom-button" onClick={() => handleClick('/Arjun')}>Arjun</Button>
+        </div>
+        <div className="col-12 mb-2">
+          <Button variant="warning" className="w-25 custom-button" onClick={() => handleClick('/Matthew')}>Matthew</Button>
+        </div>
+        <div className="col-12 mb-2">
+          <Button variant="info" className="w-25 custom-button" onClick={() => handleClick('/Mos')}>Mos</Button>
+        </div>
+        <div className="col-12 mb-2">
+          <Button className="w-25 custom-button pink-button" onClick={() => handleClick('/Arizza')}>Arizza</Button>
+        </div>
       </div>
     </div>
   );
