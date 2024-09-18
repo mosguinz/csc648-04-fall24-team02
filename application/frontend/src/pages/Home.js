167aa362122e308332cf86c529edcbd73590945b
Kiet Lam <klam11@sfsu.edu>
2024-09-18 16:22:26 -0700

    Styled Home page with bootstrap

--- application/frontend/src/pages/Home.js
+++ application/frontend/src/pages/Home.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Home.css'; // Import the CSS file
 
 function Home() {
   return (
-    <div>
-        <h1>Software Engineering Class SFSU</h1>
-        <h2>Section 04</h2>
-        <h2>Team 02</h2>
+    <div className="container text-center mt-5">
+      <div className="jumbotron">
+        <h1 className="display-4">Software Engineering Class SFSU</h1>
+        <p className="lead section-text">Section 04</p>
+        <hr className="my-4" />
+        <p className="team-text">Team 02</p>
+      </div>
     </div>
-  )
+  );
 }
- 
-export default Home
+
+export default Home;
