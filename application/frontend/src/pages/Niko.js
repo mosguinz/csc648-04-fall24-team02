b3d2316f5c887818491c69883662d854961d43d8
Arizza Cristobal <ariswooped@gmail.com>
2024-09-18 18:30:06 -0700

    Updated Niko's about me page and added img

--- application/frontend/src/pages/Niko.js
+++ application/frontend/src/pages/Niko.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import NikoImg from "../assets/Niko.png";
 
 function Niko() {
   return (
     <div>
-      <h1>Hello am Niko</h1>
+        <br /> {/* This adds an empty space before the heading */}
+        <h1>Niko Galedo</h1>
+        <h2>Front End</h2>
+        <img 
+          src={NikoImg} 
+          alt="Niko Galedo" 
+          style={{ width: '400px', height: 'auto' }} 
+        />
     </div>
   )
 }
