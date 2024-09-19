67fe043ffeb73a1acd4362c3dcf5da96b26edb34
Arizza Cristobal <ariswooped@gmail.com>
2024-09-18 18:23:19 -0700

    updated arjun's page

--- application/frontend/src/pages/Arjun.js
+++ application/frontend/src/pages/Arjun.js
@@ -4,13 +4,13 @@ import ProfilePicture from '../assets/Arjun.jpg';
 function Arjun() {
   return (
     <div>
-      <img
+        <h1>Arjun Singh Gill</h1>
+        <h2>Backend Lead</h2>
+        <img
         src={ProfilePicture}
         alt="Arjun Singh Gill"
-        style={{width: '500px', height: 'auto'}}
-      />
-      <h1>Arjun Singh Gill</h1>
-      <h2>Backend Lead</h2>
+        style={{width: '300px', height: 'auto'}}
+        />
     </div>
   )
 }
