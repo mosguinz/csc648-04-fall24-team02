76b59c1390bf6e72128f1e4652f1e17bca32d17f
nikogabeg <nikogabeg@gmail.com>
2024-09-18 17:26:21 -0700

    Upadated Niko about me page

--- application/frontend/src/pages/Niko.js
+++ application/frontend/src/pages/Niko.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import pfp from '../assets/Nikopfp.jpg'
 function Niko() {
   return (
     <div>
+      <h>Niko Galedo</h>
+      <p>Hey there I'm Niko and I am one of the front-end developers for our team!</p>
+      <img 
+        src ={pfp} 
+        style={{width: '300px', height: 'auto'}}
+      /> 
       
     </div>
   )
