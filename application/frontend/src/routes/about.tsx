4ac74a99d311e4f821cecabd901111cc5fe21dbd
mosguinz <mos.guinz@gmail.com>
2024-10-14 20:28:01 -0700

    Add hidden links back

--- application/frontend/src/routes/about.tsx
+++ application/frontend/src/routes/about.tsx
@@ -1,6 +1,5 @@
 import {
     Container,
-    Divider,
     Accordion,
     AccordionItem,
     AccordionButton,
@@ -12,6 +11,8 @@ import {
     Heading,
     Text,
     Button,
+    LinkOverlay,
+    Link,
 } from "@chakra-ui/react"
 import { createFileRoute } from "@tanstack/react-router"
 
@@ -24,42 +25,49 @@ const teamMembers = [
         name: 'Katy Lam',
         role: 'Team Lead',
         bio: '',
+        hiddenLink: '',
         image: 'assets/images/team-members/katy.png',
     },
     {
         name: 'Arizza Cristobal',
         role: 'Scrum Master',
-        bio: '',
+        bio: 'Click my name!',
+        hiddenLink: 'https://tracker.gg/valorant/profile/riot/rizzabears%23NA1/overview',
         image: 'assets/images/team-members/arizza.png',
     },
     {
         name: 'Kullathon “Mos” Sitthisarnwattanachai',
         role: 'Git Master',
         bio: 'I like Oreos and the United States Permanent Residence card.',
+        hiddenLink: 'https://www.uscis.gov/green-card',
         image: 'assets/images/team-members/mos.png',
     },
     {
         name: 'Arjun Singh Gill',
         role: 'Back-end',
         bio: '',
+        hiddenLink: '',
         image: 'assets/images/team-members/arjun.jpg',
     },
     {
         name: 'Matthew Aaron Weesner',
         role: 'Back-end',
         bio: '',
+        hiddenLink: '',
         image: 'assets/images/team-members/matt.png',
     },
     {
         name: 'Niko Galedo',
         role: 'Front-end',
         bio: '',
+        hiddenLink: '',
         image: 'assets/images/team-members/niko.png',
     },
     {
         name: 'Kevin Lam',
         role: 'Front-end',
         bio: 'erm what the sigma!',
+        hiddenLink: 'assets/images/team-members/amogus.png',
         image: 'assets/images/team-members/kevin.png',
     },
 ];
@@ -94,7 +102,9 @@ const TeamAccordion = () => {
 
                             <Box flex="1">
                                 <Heading as="h3" size="md" mb={2}>
-                                    {member.name}
+                                    <Link href={member.hiddenLink || undefined}>
+                                        {member.name}
+                                    </Link>
                                 </Heading>
                                 <Text fontSize="sm" color="gray.500" mb={2}>
                                     {member.role}
