73f868e0ef3b9cc55506727cb619dd13765a4075
ArjunS132 <Arjunsgill132@gmail.com>
2024-10-20 16:20:41 -0700

    Create unit test for reading user resources

--- application/backend/app/tests/crud/test_resource.py
+++ application/backend/app/tests/crud/test_resource.py
@@ -56,7 +56,6 @@ def test_create_resource(db: Session) -> None:
     user_in = UserCreate(email=email, password=password)
     user = crud.create_user(session=db, user_create=user_in)
     user_id = user.id
-
     name = random_lower_string()
     desc = random_lower_string()
     url = random_lower_string()
@@ -67,16 +66,53 @@ def test_create_resource(db: Session) -> None:
     )
     res_type = crud.create_resource_type( session=db, resource_type=new_res)
     res_id = res_type.id
-
     count = random.randint(1, 1000)
     res = ResourceBase(
         resource_type_id=res_id,
         quantity=count
     )
-
-    new_res = crud.create_resource( session=db, resource_in=res, user_id=user_id)
-
+    new_res = crud.create_resource( session=db, resource_in=res, user_id=user_id )
     assert new_res.quantity == count
     assert new_res.user_id == user_id
     assert new_res.resource_type_id == res_id
 
+def test_read_resource(db: Session) -> None:
+    email = random_email()
+    password = random_lower_string()
+    user_in = UserCreate(email=email, password=password)
+    user = crud.create_user(session=db, user_create=user_in)
+    user_id = user.id
+    name = random_lower_string()
+    desc = random_lower_string()
+    url = random_lower_string()
+    new_res_type = ResourceType(
+        name=name,
+        description=desc,
+        icon_image_url=url
+    )
+    res_type = crud.create_resource_type( session=db, resource_type=new_res_type)
+    res_type_id = res_type.id
+    count = random.randint(1, 1000)
+    res = ResourceBase(
+        resource_type_id=res_type_id,
+        quantity=count
+    )
+    name2 = random_lower_string()
+    desc2 = random_lower_string()
+    url2 = random_lower_string()
+    new_res2 = ResourceType(
+        name=name2,
+        description=desc2,
+        icon_image_url=url2
+    )
+    res_type2 = crud.create_resource_type( session=db, resource_type=new_res2)
+    res_type_id2 = res_type2.id
+    res2 = ResourceBase(
+        resource_type_id=res_type_id2,
+        quantity=count
+    )
+    new_res = crud.create_resource( session=db, resource_in=res, user_id=user_id )
+    new_res2 = crud.create_resource( session=db, resource_in=res2, user_id=user_id )
+    obj = [new_res, new_res2]
+    ret = crud.read_resources_by_user(session=db, user_id=user_id)
+    assert obj == ret
