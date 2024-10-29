13402b22339ec18b09bd79dc991d2f7f7d52cb39
Kevin Lam <klam11@sfsu.edu>
2024-10-28 22:34:17 -0700

    Add scene imports into main.ts to fix scene bug

--- application/frontend/src/game/main.ts
+++ application/frontend/src/game/main.ts
@@ -5,6 +5,8 @@ import MainMenu from "./scenes/MainMenu"
 import MinerPlacementScene from "./scenes/MinerPlacementScene"
 import RunningSmeltersScene from "./scenes/RunningSmeltersScene"
 import SmelterPlacementScene from "./scenes/SmelterPlacementScene"
+import BuildMenu from "./scenes/BuildMenu"
+import BaseScene from "./scenes/Base"
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -17,11 +19,13 @@ const config: Phaser.Types.Core.GameConfig = {
   backgroundColor: "#028af8",
   scene: [
     MainMenu,
+    BaseScene,
     CraftingMenu,
     InventoryMenu,
     MinerPlacementScene,
     SmelterPlacementScene,
     RunningSmeltersScene,
+    BuildMenu,
   ],
 }
 
