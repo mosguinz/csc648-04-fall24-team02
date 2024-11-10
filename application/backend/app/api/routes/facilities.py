e43f8c7ec67778e159e4fe16f4cd00cd1aa05530
ArjunS132 <Arjunsgill132@gmail.com>
2024-11-09 16:27:32 -0800

    Create update API for facilities

--- application/backend/app/api/routes/facilities.py
+++ application/backend/app/api/routes/facilities.py
@@ -73,6 +73,37 @@ def set_miners(
     return crud.read_user_miners_by_user(session=session, user_id=current_user.id)
 
 
+@router.patch(
+    "/miner",
+    response_model=list[UserMiner],
+)
+def update_miners(
+    session: SessionDep, current_user: CurrentUser, miners: list[UserMiner]
+) -> Any:
+    """
+    Set miners for the selected user
+    """
+    # TODO add in validation
+    curr_miners = crud.read_user_miners_by_user(
+        session=session, user_id=current_user.id
+    )
+    for miner in miners:
+        # grab corresponding existing resource
+        miner_id = miner.id
+        matching_miner = next(
+            (mine for mine in curr_miners if mine.id == miner_id), None
+        )
+        # if the resource already exists update else do nothing
+        if matching_miner:
+            update_miner = FacilityUpdate(
+                status=miner.status, recipe_id=miner.recipe_id
+            )
+            crud.update_user_miner(
+                session=session, db_miner=matching_miner, miner_in=update_miner
+            )
+    return crud.read_user_miners_by_user(session=session, user_id=current_user.id)
+
+
 @router.get(
     "/assembler",
     response_model=list[UserAssembler],
@@ -128,6 +159,37 @@ def set_assemblers(
             )
             crud.create_user_assembler(
                 session=session, assembler_in=assembler_in, user_id=current_user.id
+            )
+    return crud.read_user_assemblers_by_user(session=session, user_id=current_user.id)
+
+
+@router.patch(
+    "/assembler",
+    response_model=list[UserAssembler],
+)
+def update_assemblers(
+    session: SessionDep, current_user: CurrentUser, assemblers: list[UserAssembler]
+) -> Any:
+    """
+    Set assemblers for the selected user
+    """
+    # TODO add in validation
+    curr_assemblers = crud.read_user_assemblers_by_user(
+        session=session, user_id=current_user.id
+    )
+    for assembler in assemblers:
+        # grab corresponding existing resource
+        assembler_id = assembler.id
+        matching_assembler = next(
+            (mine for mine in curr_assemblers if mine.id == assembler_id), None
+        )
+        # if the resource already exists update else do nothing
+        if matching_assembler:
+            update_assembler = FacilityUpdate(
+                status=assembler.status, recipe_id=assembler.recipe_id
+            )
+            crud.update_user_assembler(
+                session=session, db_assembler=matching_assembler, assembler_in=update_assembler
             )
     return crud.read_user_assemblers_by_user(session=session, user_id=current_user.id)
 
@@ -189,3 +251,35 @@ def set_constructors(
                 session=session, constructor_in=constructor_in, user_id=current_user.id
             )
     return crud.read_user_constructors_by_user(session=session, user_id=current_user.id)
+
+
+@router.patch(
+    "/constructor",
+    response_model=list[UserConstructor],
+)
+def update_constructors(
+    session: SessionDep, current_user: CurrentUser, constructors: list[UserConstructor]
+) -> Any:
+    """
+    Set constructors for the selected user
+    """
+    # TODO add in validation
+    curr_constructors = crud.read_user_constructors_by_user(
+        session=session, user_id=current_user.id
+    )
+    for constructor in constructors:
+        # grab corresponding existing resource
+        constructor_id = constructor.id
+        matching_constructor = next(
+            (mine for mine in curr_constructors if mine.id == constructor_id), None
+        )
+        # if the resource already exists update else do nothing
+        if matching_constructor:
+            update_constructor = FacilityUpdate(
+                status=constructor.status, recipe_id=constructor.recipe_id
+            )
+            crud.update_user_constructor(
+                session=session, db_constructor=matching_constructor, constructor_in=update_constructor
+            )
+    return crud.read_user_constructors_by_user(session=session, user_id=current_user.id)
+
