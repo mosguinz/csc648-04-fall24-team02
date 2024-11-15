04c1222f1426811c95afb96d2478fd0626a5faca
Kevin Lam <klam11@sfsu.edu>
2024-11-14 23:28:38 -0800

    Add background to title screen

--- application/frontend/src/game/scenes/title-scene.ts
+++ application/frontend/src/game/scenes/title-scene.ts
@@ -6,6 +6,7 @@ export default class TestScene extends Phaser.Scene {
     }
 
     create() {
+        this.add.image(0, -400, "pink_bg").setOrigin(0, 0).setScale(0.74);
         this.add.image(100, 100, "button1");
         this.add.nineslice(500, 500, "button1",0 , 600, 400, 32, 32, 32, 32);
     }
