670445e6535e30fde59c8fefb1bf0c0f58e9ad05
Kevin Lam <klam11@sfsu.edu>
2024-11-16 11:38:13 -0800

    Add UI scene file template

---
+++ application/frontend/src/game/scenes/UI/game-menu.ts
@@ -0,0 +1,14 @@
+import Phaser from "phaser";
+import { TEXT_STYLE } from '../../config';
+import { GAME_WIDTH, GAME_HEIGHT, NSP } from '../../data/constants';
+
+export default class GameMenu extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'GameMenu',
+        });
+    }
+
+    create() {
+    }
+}
