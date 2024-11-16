3c4064a270f5bc9e40b3897eb9dd696981445505
Kevin Lam <klam11@sfsu.edu>
2024-11-15 20:44:32 -0800

    Fix bug with font

--- application/frontend/src/game/scenes/base/title-scene.ts
+++ application/frontend/src/game/scenes/base/title-scene.ts
@@ -8,7 +8,7 @@ export default class TestScene extends Phaser.Scene {
 
     create() {
         this.add.text(1100, 520, "CLICK AND MORTAR", TITLE_STYLE).setOrigin(0.5, 0.5);
-        this.add.nineslice(1100, 720, "button1",0 , 550, 120, 32, 32, 32, 32).setOrigin(0.5, 0.5).setTint(0xEF9DE9);
+        this.add.nineslice(1100, 720, "button1",0 , 550, 120, 32, 32, 32, 32).setOrigin(0.5, 0.5);
 
 
         
