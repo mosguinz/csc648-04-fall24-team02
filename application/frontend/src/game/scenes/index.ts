734bdf4b68e76fc069f5153b26166787b888a245
Kevin Lam <klam11@sfsu.edu>
2024-11-17 00:19:26 -0800

    Change some names

--- application/frontend/src/game/scenes/index.ts
+++ application/frontend/src/game/scenes/index.ts
@@ -1,7 +1,7 @@
 // import scenes here instead of main.ts
-import TitleScene from './base/titleScene';
+import TitleScene from './base/titleScreen';
 import PreloadScene from './preloadScene';
-import MainGameScene from './base/mainGameScene';
+import MainGameScene from './base/mainGame';
 import GameMenu from './ui/gameMenu';
 import InventoryMenu from './ui/inventoryMenu';
 import BuildMenu from './ui/buildMenu';
