d7dd727c56c39fed5a2b866972e3ae9dcb0d1cd9
Kevin Lam <klam11@sfsu.edu>
2024-11-17 20:49:28 -0800

    Implement logic for crafter hook

--- application/frontend/src/game/hooks/crafter.ts
+++ application/frontend/src/game/hooks/crafter.ts
@@ -1,61 +1,58 @@
 import Phaser from "phaser";
-// import { GameData } from "../stores/gameData";
-// import { UserMiner } from "../../client";
+import { GameData } from "../stores/gameData";
+import { UserAssembler } from "../../client";
 
 export default class Crafter extends Phaser.Scene {
-    // private crafterTimers: { [key: string]: Phaser.Time.TimerEvent } = {};
+    private crafterTimers: { [key: string]: Phaser.Time.TimerEvent } = {};
 
     constructor() {
-        super({ key: "Crafter"});
+        super({ key: "Crafter" });
     }
 
-    create()  {
-    //     GameData.populateMiners().then(() => {
-    //         this.startMining();
-    //     });
+    create() {
+        GameData.populateCrafters().then(() => {
+            this.startCrafting();
+        });
 
-    //     // Listen for miner updates
-    //     this.events.on("add-miner", (newMiner: UserMiner) => {
-    //         console.log("New Miner Added:", newMiner);
-    //         this.addMiner(newMiner);
-    //     });
-    // }
+        // Listen for miner updates
+        this.events.on("add-crafter", (newCrafter: UserAssembler) => {
+            console.log("New Crafter Added:", newCrafter);
+            this.addCrafter(newCrafter);
+        });
+    }
 
-    // private startMining(): void {
-    //     for (const miner of GameData.miners) {
-    //         this.startMinerTimer(miner);
-    //     }
-    // }
+    private startCrafting(): void {
+        for (const assembler of GameData.miners) {
+            this.startCrafterTimer(assembler);
+        }
+    }
 
-    // private addMiner(miner: UserMiner): void {
-    //     console.log(`Adding miner ${miner.id}`);
-    //     if (GameData.miners.find((m) => m.id === miner.id)) {
-    //         console.warn(`Miner already exists.`);
-    //         return;
-    //     }
+    private addCrafter(crafter: UserAssembler): void {
+        if (GameData.crafters.find((m) => m.id === crafter.id)) {
+            return;
+        }
 
-    //     GameData.miners.push(miner);
-    //     GameData.nextMinerId++;
-    //     GameData.saveMiners();
+        GameData.miners.push(crafter);
+        GameData.nextMinerId++;
+        GameData.saveMiners();
 
-    //     this.startMinerTimer(miner);
+        this.startCrafterTimer(crafter);
 
-    // }
+    }
 
-    // private startMinerTimer(miner: UserMiner): void {
-    //     if (this.minerTimers[miner.id || ""]) {
-    //         return;
-    //     }
+    private startCrafterTimer(miner: UserAssembler): void {
+        if (this.crafterTimers[miner.id || ""]) {
+            return;
+        }
 
-    //     this.minerTimers[miner.id || ""] = this.time.addEvent({
-    //         delay: 5000,
-    //         loop: true,
-    //         callback: () => {
-    //             if (miner.recipe_id) {
-    //                 console.log(`Mining with miner ${miner.id}`);
-    //                 GameData.addResource(miner.recipe_id, 1);
-    //             }
-    //         },
-    //     });
+        this.crafterTimers[miner.id || ""] = this.time.addEvent({
+            delay: 5000,
+            loop: true,
+            callback: () => {
+                if (miner.recipe_id) {
+                    console.log(`CRAFT!`);
+                }
+            },
+        });
     }
 }
